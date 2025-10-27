3a568a2066aef61620be135000cddc18f6da1c7c
fuwuffy <fuwuffyderg@proton.me>
2025-10-27 15:54:23 +0100

    Change svg format to pdf

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment with Inkscape";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux"; # adjust if needed
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        inkscape
+        ghostscript
+        fontconfig
+      ];
+    };
+  };
+}
