c5c65cce83297c98be7bf40fd1f95c1bb07c6dce
fuwuffy <fuwuffyderg@proton.me>
2025-11-03 13:43:10 +0100

    Code format listings

--- listings/renderer_factory.cpp
+++ listings/renderer_factory.cpp
@@ -1,10 +1,11 @@
-std::unique_ptr<IRenderer> RendererFactory::CreateRenderer(const GraphicsAPI api, Window* const win) {
-   switch (api) {
-      case GraphicsAPI::OpenGL:
-         return std::make_unique<GLRenderer>(win);
-      case GraphicsAPI::Vulkan:
-         return std::make_unique<VulkanRenderer>(win);
-      default:
-         throw std::runtime_error("Unsupported Graphics API");
-   }
+std::unique_ptr<IRenderer>
+RendererFactory::CreateRenderer(const GraphicsAPI api, Window *const win) {
+  switch (api) {
+  case GraphicsAPI::OpenGL:
+    return std::make_unique<GLRenderer>(win);
+  case GraphicsAPI::Vulkan:
+    return std::make_unique<VulkanRenderer>(win);
+  default:
+    throw std::runtime_error("Unsupported Graphics API");
+  }
 }
